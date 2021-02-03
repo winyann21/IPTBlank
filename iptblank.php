da6033547fada3b136e9bce8bdaedba73d0faad2
Sherwin Romero <it.sherwinromero@gmail.com>
2021-02-03 18:54:48 +0800

    IPT313 Group Blank

---
+++ iptblank.php
@@ -0,0 +1,9 @@
+<?php
+    echo "IPT 313 Group Blank";
+    echo "Leader: Sherwin Romero";
+    echo "Members:";
+    echo "John Mico Paule";
+    echo "Mark Edison Pasion";
+    echo "Glady Mae Salenga";
+    echo "Alyssa Ronquillo";
+?>
